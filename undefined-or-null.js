73bb6ba6d2d706ad6bd94274e2eac19e7a99e84e
sheiktanvirulislam <tanvirulislam@gmail.com>
2021-07-26 16:32:07 +0600

    commited 2s

--- undefined-or-null.js
+++ undefined-or-null.js
@@ -1,18 +1,18 @@
-// let name ;
-// function add(num1,num2){
-//    console.log(num1 + num2);
-// return 
-// }
+let name ;
+function add(num1,num2){
+   console.log(num1 + num2);
+return 
+}
 
 
-// function add(num1,num2) {
-//     console.log(num2);
-// }
-// const result = add(12)
-// const person = {name:"rohan",phone:907465}; 
-// console.log(person.address); 
-// let fun = undefined;
-// console.log(fun);
+function add(num1,num2) {
+    console.log(num2);
+}
+const result = add(12)
+const person = {name:"rohan",phone:907465}; 
+console.log(person.address); 
+let fun = undefined;
+console.log(fun);
 let number1 = 12;
 
 
@@ -22,14 +22,14 @@ let number1 = 12;
 
 
 
-// function palindrome(word) {
-//     // let word1 = word + '';
-//     let word2 = word.split('').reverse().join('');
-//     if (word2 == word) {
-//         return true;
-//     }
-//     else {
-//         return false;
-//     }
-// }
-// console.log(palindrome(''))
+function palindrome(word) {
+    // let word1 = word + '';
+    let word2 = word.split('').reverse().join('');
+    if (word2 == word) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+console.log(palindrome(''))
