56f899a0138d94e8dae02513c5f291d326003a79
sheiktanvirulislam <tanvirulislam@gmail.com>
2021-07-26 16:05:33 +0600

    commit

---
+++ ep-1-conditonal-true-false.js
@@ -0,0 +1,25 @@
+// 0 , "",undefined,null , false these are falsy value
+//truth- "0",' ',
+
+// const name1 = "tanvir";
+let name1 = 'false';
+console.log(name1)
+if (name1) {
+    console.log("true")
+}
+else {
+    console.log("false")
+}
+
+
+
+
+
+
+
+// if (name1>=18) {
+//     console.log("you can vote")
+// }
+// else {
+//     console.log("you can't vote")
+// }
